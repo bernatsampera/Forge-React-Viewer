1b0072b028b16bb58bba481f6012000b1db803c3
bernatsampera <b.sampera@gmail.com>
2019-03-12 10:24:14 +0100

    fix server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/modelderivative", modelderivative);
 app.use("/api/designautomation", designautomation);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(Express.static("client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
